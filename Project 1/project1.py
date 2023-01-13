deb12b7e70a2a399420067d2448e873fee3aba5b
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2023-01-12 19:21:45 -0600

    Update project1.py

--- Project 1/project1.py
+++ Project 1/project1.py
@@ -3,9 +3,8 @@ import random
 import numpy as np
 
 df = pd.read_csv('Project 1/bid_data.csv')
-our_bids = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 
-def exact_winning_prob_utility(our_bid, value):
+def exact_win_prob_utility(our_bid, value):
     # calculate winning prob 
     wins = 0
     for col_header in df.head():
@@ -25,7 +24,7 @@ def exact_winning_prob_utility(our_bid, value):
 
     return win_prob, expected_utility
 
-def evaluate_our_bids_exact():
+def evaluate_our_bids_exact(our_bids):
     bid_results = [0 for i in range(len(our_bids))]
     for i in range(len(our_bids)):
         win_prob, expected_utility = exact_win_prob_utility(our_bids[i], (i+1)*10)
@@ -62,8 +61,6 @@ def evaluate_optimal_bids_exact():
 
 ## Functions for evaluating bids using all/exact data
 def monte_carlo_win_prob_utility(our_bid, value):
-    # from the value col in excel draw a random bid 
-    
     alpha = 0.05
     epsilon = 0.15
     n = int((np.log(2 / alpha)) / (2 * (pow(epsilon, 2))))
@@ -74,6 +71,7 @@ def monte_carlo_win_prob_utility(our_bid, value):
     df_flat = df_as_matrix.flatten()
 
     for i in range(n):
+        # draw a random bid
         opponent_bid = np.random.choice(df_flat) 
         if our_bid > float(opponent_bid):
             wins += 1
@@ -89,7 +87,7 @@ def monte_carlo_win_prob_utility(our_bid, value):
 
     return win_prob, expected_utility
    
-def evaluate_our_bids_monte_carlo():
+def evaluate_our_bids_monte_carlo(our_bids):
     bid_results = [0 for i in range(len(our_bids))]
     for i in range(len(our_bids)):
         win_prob, expected_utility = monte_carlo_win_prob_utility(our_bids[i], (i+1)*10)
@@ -120,32 +118,62 @@ def evaluate_optimal_bids_monte_carlo():
     # print("MC optimal bid results")
     # print(value_results)
     return value_results
-    
-    
-our_exact = evaluate_our_bids_exact()
-opt_exact = evaluate_optimal_bids_exact()
-our_mc = evaluate_our_bids_monte_carlo()
-opt_mc = evaluate_optimal_bids_monte_carlo()
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-output = {'value': [10*(i+1) for i in range(10)], 
-        'Our Bids Exact: Bid': column(our_exact, 0),
-        'Our Bids Exact: EU': column(our_exact, 1),
-        'Our Bids Exact: WP': column(our_exact, 2),
-        '': ['' for i in range(10)],
-        'Optimal Bids Exact: Bid': column(opt_exact, 0),
-        'Optimal Bids Exact: EU': column(opt_exact, 1),
-        'Optimal Bids Exact: WP': column(opt_exact, 2),
-        ' ': ['' for i in range(10)],
-        'Our Bids MC: Bid': column(our_mc, 0),
-        'Our Bids MC: EU': column(our_mc, 1),
-        'Our Bids MC: WP': column(our_mc, 2),
-        '  ': ['' for i in range(10)],
-        'Optimal Bids MC: Bid': column(opt_mc, 0),
-        'Optimal Bids MC: EU': column(opt_mc, 1),
-        'Optimal Bids MC: WP': column(opt_mc, 2)}
+def part1():
+    our_bids = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    our_exact = evaluate_our_bids_exact(our_bids)
+    opt_exact = evaluate_optimal_bids_exact()
+    our_mc = evaluate_our_bids_monte_carlo(our_bids)
+    opt_mc = evaluate_optimal_bids_monte_carlo()
 
-output_df = pd.DataFrame(output)
-output_df.to_csv('Project 1/bid_data_results_new.csv', index=False)
+    output = {'value': [10*(i+1) for i in range(10)], 
+            'Our Bids Exact: Bid': column(our_exact, 0),
+            'Our Bids Exact: E[U]': column(our_exact, 1),
+            'Our Bids Exact: Pr[W]': column(our_exact, 2),
+            '': ['' for i in range(10)],
+            'Optimal Bids Exact: Bid': column(opt_exact, 0),
+            'Optimal Bids Exact: E[U]': column(opt_exact, 1),
+            'Optimal Bids Exact: Pr[W]': column(opt_exact, 2),
+            ' ': ['' for i in range(10)],
+            'Our Bids MC: Bid': column(our_mc, 0),
+            'Our Bids MC: E[U]': column(our_mc, 1),
+            'Our Bids MC: Pr[W]': column(our_mc, 2),
+            '  ': ['' for i in range(10)],
+            'Optimal Bids MC: Bid': column(opt_mc, 0),
+            'Optimal Bids MC: E[U]': column(opt_mc, 1),
+            'Optimal Bids MC: Pr[W]': column(opt_mc, 2)}
+
+    output_df = pd.DataFrame(output)
+    output_df.to_csv('Project 1/bid_data_results_new.csv', index=False)
+
+def find_optimal_bids(data):
+    values = [10*(i+1) for i in range(10)]
+    optimal_bids = [0 for i in range(10)]
+    
+    for i in range(len(values)):
+        # use data to figure out a value somehow
+        # could just be to do what we did for part 1
+        optimal_bids[i] = 0 # bid
+
+    return optimal_bids
+
+def compare_bids(a, b):
+    # maybe return average difference between expected utilities?
+    return []
+
+def part2():
+    exact_optimal_bids = [6, 12, 12, 21, 22, 31, 32, 41, 41, 52]
+    results = [0 for i in range(df.shape[0])]
+
+    for i in range(len(results)):
+        # get random subset of data of size i
+        subset = df.sample(n = i) 
+        subset_optimal_bids = find_optimal_bids(subset)
+        results[i] = compare_bids(exact_optimal_bids, subset_optimal_bids)
+
+    # plot results vs i
+    # determine best fit line
+    # we should expect to see the results getting lower as i increases
