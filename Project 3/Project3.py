a5d4bb6fd3c4d9773636019dddcaaa63ff9d1db5
sesawyer1 <sarahsawyer2024@u.northwestern.edu>
2023-02-08 16:05:31 -0600

    update

--- Project 3/Project3.py
+++ Project 3/Project3.py
@@ -1,5 +1,8 @@
 import numpy as np
 import nashpy as nash
+import math
+import matplotlib.pyplot as plt
+
 # inputs: 
 # v, an array of payoffs for each action at a given turn
 # epsilon, the learning rate
@@ -13,6 +16,7 @@ def exponential_weights(v, epsilon, h):
     # because we're referencing the previous column, the first
     # column of ones gets removed so we need to add it back
     weights = np.insert(weights, 0, np.ones(v.shape[0]), axis=1)
+    # print(f"SUM:{np.sum(weights, axis=0)}" )
     pi = np.divide(weights, np.sum(weights, axis=0))
     return weights, pi
 
@@ -22,27 +26,33 @@ def bimatrix_game(A, B):
     eq = game.support_enumeration()
     return eq #returns optimal pick for each player 
 
+# generates a random bi-matrix game
 def generate_random_game():
-    A = np.random.rand(2, 2)
-    B = np.random.rand(2, 2)
+    A = np.random.randint(0, 10, size=(2, 2))
+    B = np.random.randint(0, 10, size=(2, 2))
     return A, B
 
-def test_exponential_weights(A, B):
+# EW tests that return probabilities for all actions/rounds 
+def test_exponential_weights(A, B, epsilon=0.1):
     ROUNDS = 25
-    EPSILON = 1
-    payoffs_A = np.empty([2, ROUNDS])
-    payoffs_B = np.empty([2, ROUNDS])
+    payoffs_A = np.zeros([2, ROUNDS])
+    payoffs_B = np.zeros([2, ROUNDS])
     for round in range(ROUNDS):
-        _, pi_A = exponential_weights(payoffs_A[:, 0:round+1], EPSILON, 1)
-        _, pi_B = exponential_weights(payoffs_B[:, 0:round+1], EPSILON, 1)
+        _, pi_A = exponential_weights(payoffs_A[:, 0:round+1], epsilon, 1)
+        _, pi_B = exponential_weights(payoffs_B[:, 0:round+1], epsilon, 1)
         action_A = random_pick(pi_A[:, round])
         action_B = random_pick(pi_B[:, round])
-        payoffs_A[action_A][round] = A[action_A][action_B]
-        payoffs_B[action_B][round] = B[action_A][action_B]
-    _, pi_A = exponential_weights(payoffs_A, EPSILON, 1)
-    _, pi_B = exponential_weights(payoffs_B, EPSILON, 1)
+        payoffs_A[0][round] = A[0][action_B]
+        payoffs_A[1][round] = A[1][action_B]
+        # print(f"B {B[action_A][action_B]}")
+        payoffs_B[0][round] = B[action_A][0]
+        payoffs_B[1][round] = B[action_A][1]
+        # print(f"B Payoffs {payoffs_B}")
+    _, pi_A = exponential_weights(payoffs_A, epsilon, 1)
+    _, pi_B = exponential_weights(payoffs_B, epsilon, 1)
     return pi_A, pi_B
 
+# randomly pick an index in the array according to the given probabilities in each element of the array
 def random_pick(probability_array):
     random_number = np.random.random()
     cumulative_probability = 0.0
@@ -52,10 +62,48 @@ def random_pick(probability_array):
             return index
     return len(probability_array) - 1
 
-A, B = generate_random_game()
-pi_A, pi_B = test_exponential_weights(A, B)
-eq = bimatrix_game(A, B)
-print(A, B)
-for i in eq:
-    print(i)
-print(pi_A[:, 24], pi_B[:, 24])
+# Monte Carlo tests to determine if EW converges to the nash equilibrium
+def monte_carlo_EW(epsilon):
+    N = 200
+    converges_to_nash_count = 0
+    for i in range(N):
+        A, B = generate_random_game()
+        pi_A, pi_B = test_exponential_weights(A, B, epsilon)
+        eq = bimatrix_game(A, B)
+        for i in eq:
+            # test if the results from exponential weights are within a reasonable threshold of the nash equilibrium
+            a_true_nash = i[0]
+            a_results = pi_A[:, 24]
+            a_converges = (abs(a_true_nash[0] - np.round(a_results[0])) < 0.1) & (abs(a_true_nash[1] - np.round(a_results[1])) < 0.1)
+            b_true_nash = i[1]
+            b_results = pi_B[:, 24]
+            b_converges = (abs(b_true_nash[0] - np.round(b_results[0])) < 0.1) & (abs(b_true_nash[1] - np.round(b_results[1])) < 0.1)            
+            if a_converges & b_converges:
+                converges_to_nash_count += 1
+        # print(pi_A[:, 24], pi_B[:, 24])
+    # print (converges_to_nash_count/N)
+    return converges_to_nash_count/N
+
+#strategy: look at the nash eq --> always pick the one where we have the highest payoff
+def beat_learning_alg(A, B):
+    game = nash.game(A,B)
+    eq = game.support_enumeration()
+    if eq[1][0] == 1:
+        col = 0 
+    else:
+        col = 1
+
+    play = max(B[0][col], B[1][col])
+    return play 
+
+def test_learning_rates_for_EW():
+    epsilons = [i/10 for i in range(500)]
+    results = [0 for i in range(len(epsilons))]
+    for i, e in enumerate(epsilons):
+        results[i] = monte_carlo_EW(e)
+    plt.plot(epsilons, results)
+    plt.xlabel('Epsilon - Learning Rate')
+    plt.ylabel('Convergence to Nash Rate')
+    plt.show()
+    
+test_learning_rates_for_EW()
